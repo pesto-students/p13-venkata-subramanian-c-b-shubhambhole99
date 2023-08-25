70358eb3d3e8c59ad9309dd180251a49b0d2566b
unknown <shubhambhole99>
2023-08-25 12:50:05 +0530

    scopes,global objects

---
+++ Week-4/Global Scopes,Objects/Loops/index.js
@@ -0,0 +1,15 @@
+
+
+function drawTriangle(){
+        for (let i = 1; i <= rows; i++) {
+            let row = '';
+            for (let j = 1; j <= i; j++) {
+                row += '*';
+            }
+            console.log(row);
+        }
+    }
+
+
+
+    drawTriangle(4)
